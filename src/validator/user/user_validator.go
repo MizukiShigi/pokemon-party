8398e94aa65fbe45621259bb22c3dcc64f463f51
MizukiShigihara <mizuki.shigihara@zozo.com>
2024-01-13 17:24:44 +0900

    バリデーション処理追加

--- src/validator/user/user_validator.go
+++ src/validator/user/user_validator.go
@@ -8,6 +8,7 @@ import (
 
 type IUserValidator interface {
 	GetUserValidate(user domain.User) error
+	CreateUserValidate(user domain.User) error
 }
 
 type userValidator struct{}
@@ -20,6 +21,19 @@ func (uv *userValidator) GetUserValidate(user domain.User) error {
 	validate := validator.New()
 	if err := validate.Var(user.ID, "required"); err != nil {
 		myError := domain.NewMyError(domain.InvalidInput, "user_id")
+		return myError
+	}
+	return nil
+}
+
+func (uv *userValidator) CreateUserValidate(user domain.User) error {
+	validate := validator.New()
+	if err := validate.Struct(user); err != nil {
+		var errFields string
+		for _, err := range err.(validator.ValidationErrors) {
+			errFields += err.Field()
+		}
+		myError := domain.NewMyError(domain.InvalidInput, errFields)
 		return myError
 	}
 	return nil
